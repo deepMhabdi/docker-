eef2f91748484a360a63c779b1c6ba8c98cee4dc
Deep Mhabdi <dmmhabdi2701@gmail.com>
2025-08-11 23:22:29 +0530

    fix: correct spelling of 'express' in server.js and remove duplicate username field in index.html

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,5 +1,5 @@
-const expres = require('express');
-const app = expres();
+const express = require('express');
+const app = express();
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 
