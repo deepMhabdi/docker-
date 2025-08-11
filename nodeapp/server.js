23c2cf041eb737ab9d90df52920d3da5e5f04375
Deep Mhabdi <dmmhabdi2701@gmail.com>
2025-08-11 23:18:25 +0530

    updated server.js file

---
+++ nodeapp/server.js
@@ -0,0 +1,44 @@
+const expres = require('express');
+const app = expres();
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+
+const PORT = 3030;
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));   
+})
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+//Get all users
+app.get("/getUsers", async (req, res) => {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("my-sample-db");
+    const data = await db.collection("users").find({}).toArray();
+
+    client.close();
+    res.send(data);
+})
+
+//Post new users
+app.post("/addUser", async(req, res) =>{
+    const userObj = req.body;
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("my-sample-db");
+    const data = await db.collection("users").insertOne(userObj);
+    console.log(data)
+    console.log("data inserted");
+    client.close();
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
